38c5db17cc9a43383b0da68eda9fe6ca82dec267
Kmc1319 <158459353+Kmc1319@users.noreply.github.com>
2024-04-05 12:18:14 +0200

    Pantalla registro

--- demo/src/main/java/com/example/SecondaryController.java
+++ demo/src/main/java/com/example/SecondaryController.java
@@ -1,10 +1,28 @@
 package com.example;
 
 import java.io.IOException;
+import java.util.ResourceBundle;
+
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class SecondaryController {
+public class SecondaryController implements Initializable {
 
+    @FXML
+    private ImageView imagenLogo;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        // Cargar la imagen desde el archivo
+        Image imagen = new Image(getClass().getResourceAsStream("/com/example/imgs/logo.jpg"));
+        
+        // Establecer la imagen en el ImageView
+        imagenLogo.setImage(imagen);
+    }
     @FXML
     private void switchToPrimary() throws IOException {
         App.setRoot("/com/example/vistas/primary");
