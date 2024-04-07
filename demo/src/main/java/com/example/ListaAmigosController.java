80327ba7ae5c43873308ea2b0690337f64bd6a14
Nicolas <841142@unizar.es>
2024-04-07 12:56:31 +0200

    Lista de amigos

--- demo/src/main/java/com/example/ListaAmigosController.java
+++ demo/src/main/java/com/example/ListaAmigosController.java
@@ -1,13 +1,25 @@
 package com.example;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
 public class ListaAmigosController implements Initializable{
 
     @Override
     public void initialize(URL arg0, ResourceBundle arg1) {
+    }
+
+    @FXML
+    private void switchToPerfil() throws IOException {
+        App.setRoot("/com/example/vistas/perfil");
+    }
+
+    @FXML
+    private void switchToMainMenu() throws IOException {
+        App.setRoot("/com/example/vistas/menuPrincipal");
     }
 }
