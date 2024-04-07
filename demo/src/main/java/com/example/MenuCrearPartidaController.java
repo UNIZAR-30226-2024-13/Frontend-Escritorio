75f670eea9e7df19ab155a06e1f501fc898a2bf7
Kmc1319 <158459353+Kmc1319@users.noreply.github.com>
2024-04-07 13:01:13 +0200

    Menu desplegable y menus de elegir juego

--- demo/src/main/java/com/example/MenuCrearPartidaController.java
+++ demo/src/main/java/com/example/MenuCrearPartidaController.java
@@ -1,13 +1,55 @@
 package com.example;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.event.ActionEvent;
+import javafx.scene.layout.VBox;
+
 
 public class MenuCrearPartidaController implements Initializable{
 
+    @FXML
+    private VBox opcionesVBox;
+
+    private boolean opcionesVisible = false;
+
     @Override
     public void initialize(URL arg0, ResourceBundle arg1) {
+    }
+
+    @FXML
+    private void switchToAmigos() throws IOException {
+        App.setRoot("/com/example/vistas/menuAmigos");
+    }
+
+    @FXML
+    private void switchToMenuCrearPartida() throws IOException {
+        App.setRoot("/com/example/vistas/menuCrearPartida");
+    }
+    
+    @FXML
+    private void switchToMenuUnirse() throws IOException {
+        App.setRoot("/com/example/vistas/menuUnirsePartida");
+    }
+
+    @FXML
+    private void switchToMenuPartidas() throws IOException {
+        App.setRoot("/com/example/vistas/menuPartidasPausadas");
+    }
+
+    @FXML
+    private void switchToPerfil() throws IOException {
+        App.setRoot("/com/example/vistas/perfil");
+    }
+
+    @FXML
+    private void mostrarOcultarOpciones(ActionEvent event) {
+        opcionesVisible = !opcionesVisible;
+        opcionesVBox.setManaged(opcionesVisible);
+        opcionesVBox.setVisible(opcionesVisible);
     }
 }
--- demo/src/main/java/com/example/MenuUnirsePartidaController.java
+++ demo/src/main/java/com/example/MenuUnirsePartidaController.java
@@ -1,13 +1,41 @@
 package com.example;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+
 
 public class MenuUnirsePartidaController implements Initializable{
 
     @Override
     public void initialize(URL arg0, ResourceBundle arg1) {
+    }
+
+    @FXML
+    private void switchToAmigos() throws IOException {
+        App.setRoot("/com/example/vistas/menuAmigos");
+    }
+
+    @FXML
+    private void switchToMenuCrearPartida() throws IOException {
+        App.setRoot("/com/example/vistas/menuCrearPartida");
+    }
+    
+    @FXML
+    private void switchToMenuUnirse() throws IOException {
+        App.setRoot("/com/example/vistas/menuUnirsePartida");
+    }
+
+    @FXML
+    private void switchToMenuPartidas() throws IOException {
+        App.setRoot("/com/example/vistas/menuPartidasPausadas");
+    }
+
+    @FXML
+    private void switchToPerfil() throws IOException {
+        App.setRoot("/com/example/vistas/perfil");
     }
 }
