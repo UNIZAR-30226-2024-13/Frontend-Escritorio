a99cd4a5801d75a8af6d3dacb4891e2c330ec109
Kmc1319 <158459353+Kmc1319@users.noreply.github.com>
2024-04-27 18:01:27 +0200

    Reverso dinámico bj

--- demo/src/main/java/com/example/juegos/BlackjackController.java
+++ demo/src/main/java/com/example/juegos/BlackjackController.java
@@ -14,6 +14,8 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 
 public class BlackjackController implements Initializable{
@@ -33,6 +35,15 @@ public class BlackjackController implements Initializable{
 
     @FXML
     private GridPane quintaCartaMesa;
+
+    @FXML
+    private GridPane cartasUsuario2;
+
+    @FXML
+    private GridPane cartasUsuario3;
+    
+    @FXML
+    private GridPane cartasUsuario4;
 
     @FXML
     private GridPane cartas;
@@ -56,6 +67,9 @@ public class BlackjackController implements Initializable{
                 n++;
             }
             cartas.setHgap(10);
+            cartasUsuario2.setHgap(30);
+            cartasUsuario3.setVgap(30);
+            cartasUsuario4.setVgap(30);
             // Eliminar lo que hubiera antes y crear botones para cada carta
             cartas.getChildren().clear();
             for (CartaFrancesa carta : listaCartas) {
@@ -63,6 +77,30 @@ public class BlackjackController implements Initializable{
                     Button boton = new Button(carta.toString());
                     boton.getStyleClass().add("carta-button");
                     cartas.add(boton, numCartas, 0);
+
+                    ImageView imagenRev = new ImageView();
+                    Image imagen = new Image(getClass().getResourceAsStream("/com/example/imgs/reverso.jpg"));
+            
+                    imagenRev.setImage(imagen);
+                    imagenRev.setFitWidth(40);
+                    imagenRev.setFitHeight(60);
+
+                    ImageView imagenRev2 = new ImageView();
+            
+                    imagenRev2.setImage(imagen);
+                    imagenRev2.setFitWidth(40);
+                    imagenRev2.setFitHeight(60);
+
+                    ImageView imagenRev3 = new ImageView();
+            
+                    imagenRev3.setImage(imagen);
+                    imagenRev3.setFitWidth(40);
+                    imagenRev3.setFitHeight(60);
+
+                    cartasUsuario2.add(imagenRev, numCartas, 0);
+                    cartasUsuario3.add(imagenRev2, 0, numCartas);
+                    cartasUsuario4.add(imagenRev3, 0, numCartas);
+
                     numCartas++;
                 }
             }
